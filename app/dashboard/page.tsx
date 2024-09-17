97fc3714a4c3bf7ec48fa1ffac3a05f56560e767
rohansinha01 <rohansinha01@gmail.com>
2024-09-16 21:53:14 -0400

    setting up front end

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import Main from "@/components/Main";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "TV Playlists ⋅ Dashboard",
+   
+  };
+
+export default function Home() {
+    
+    const isAuthenticated = false; 
+    return (
+      <Main>
+    
+      </Main>
+    );
+  }
---
+++ components/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = (props: any) => {
+  const { children } = props
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default Main
